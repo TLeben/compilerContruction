09546768ab5b1d39b350386d64d7ce003c95293e
Marc Schweikert <schweikm@gmail.com>
2015-01-17 07:59:20 -0700

    adding regression test capability

---
+++ test/run_all_tests.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -o nounset
+
+echo -e "\n\n---->  BEGIN TESTING AT $(date --utc)  <----\n\n"
+
+count=1
+while [[ 1 -eq 1 ]];
+do
+	if [[ -d test${count} ]]; then
+		pushd test${count} > /dev/null
+			echo "--------------------"
+			echo "-- TEST ${count} --"
+			echo "--------------------"
+
+			rm -f test${count}.result
+			./test${count}.py < test${count}.in > test${count}.result
+			diff test${count}.out test${count}.result > /dev/null 2>&1
+
+			if [[ $? -eq 0 ]]; then
+				echo "TEST ${count} PASSED!"
+			else
+				echo "TEST ${count} FAILED!"
+			fi
+
+			rm -f test${count}.result
+			count=$((${count} + 1))
+		popd > /dev/null
+	else
+		echo "All tests run"
+		break
+	fi
+done
+echo -e "\n\n---->  END   TESTING AT $(date --utc)  <----\n\n"
