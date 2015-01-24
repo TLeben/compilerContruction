ba760a0ecf146c8cbca08fef2caf13950ffa85cd
Marc Schweikert <schweikm@gmail.com>
2015-01-24 12:30:10 -0700

    reorganized test structure.  added new input test

---
+++ test/hw1/input/test6/test6.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+input()
+a1 = input()
+print a1
+
+print input()
+
+a2 = input()
+print a2
+
+a3 = - input()
+print a3
+
+a4 = input() + 2
+print a4
+
+a5 = 2 + input()
+print a5
+
+a6 = 2 + - input()
+print a6
+
+a7 = - input() + 10
+print a7
+
+a8 = input() + input()
+print a8
+
+a9 = - input() + input()
+print a9
+
--- test/hw1/run_all_tests.sh
+++ test/hw1/run_all_tests.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -o nounset
 
-readonly COMPILER_DIR="$(pwd)/.."
-readonly RUNTIME_DIR="$(pwd)/../runtime"
+readonly COMPILER_DIR="${1:-}"
+readonly RUNTIME_DIR="${2:-}"
 
-for sub_dir in hw1
+for sub_dir in arithmetic \
+               input
 do
     echo
     echo "--------->  BEGIN ${sub_dir}  <----------"
 	pushd ${sub_dir} > /dev/null
-		./run_all_tests.sh ${COMPILER_DIR} ${RUNTIME_DIR}
+		./run_tests.sh ${COMPILER_DIR} ${RUNTIME_DIR}
 	popd > /dev/null
     echo "--------->  END ${sub_dir}  <----------"
     echo
