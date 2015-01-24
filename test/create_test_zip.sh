c695a46b8d281176a74c9eedcf17ff25f2a6aebc
Marc Schweikert <schweikm@gmail.com>
2015-01-24 12:43:03 -0700

    fixing test zip creation script

--- test/create_test_zip.sh
+++ test/create_test_zip.sh
@@ -7,20 +7,25 @@ tmp_dir="/tmp/test_zip"
 rm -fr ${tmp_dir}
 mkdir -p ${tmp_dir}
 
-for sub_dir in arithmetic \
-               input
+for hw_dir in hw1
 do
-	pushd ${sub_dir} > /dev/null
-		count=1
-		while [[ 1 -eq 1 ]]
+	pushd ${hw_dir} > /dev/null
+		for sub_dir in $(ls -d */)
 		do
-			if [[ -d test${count} ]]; then
-				cp -f test${count}/test${count}.py ${tmp_dir}/${sub_dir}_test${count}.py
-				cp -f test${count}/test${count}.in ${tmp_dir}/${sub_dir}_test${count}.in
-			else
-				break
-			fi
-			count=$((${count} + 1))
+			sub_dir=${sub_dir%/}
+			pushd ${sub_dir} > /dev/null
+				count=1
+				while [[ 1 -eq 1 ]]
+				do
+					if [[ -d test${count} ]]; then
+						cp -f test${count}/test${count}.py ${tmp_dir}/${hw_dir}_${sub_dir}_test${count}.py
+						cp -f test${count}/test${count}.in ${tmp_dir}/${hw_dir}_${sub_dir}_test${count}.in
+					else
+						break
+					fi
+					count=$((${count} + 1))
+				done
+			popd > /dev/null
 		done
 	popd > /dev/null
 done
