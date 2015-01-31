89a7cad7b8f3dcad32ae9de59222eb83d551127c
Marc Schweikert <schweikm@gmail.com>
2015-01-30 20:36:13 -0700

    add grammar clarification

--- hw2/parse.py
+++ hw2/parse.py
@@ -12,48 +12,41 @@ class Parser:
         ('left','PLUS')
         )   
 
+    #
+    # grammar rule
+    #   simple_statement ::= "print" expression
+    #
     def p_print_statement(self, t):
         'statement : PRINT expression'
         if self.debug >= 1: print "got print"
         t[0] = Printnl(t[2], None)
 
 
+    #
+    # grammar rule
+    #   simple_statement ::= name "=" expression
+    #
     def p_assign_statement(self, t):
         'statement : ID ASSIGN expression'
         if self.debug >= 1: print "got assign"
         self.ids[t[1]] = t[3]
         t[0] = Assign(AssName(t[1], 'OP_ASSIGN'), t[3])
 
+
+    #
+    # grammar rule
+    #   simple_statement ::= expression
+    #
     def p_statement_expr(self, t):
         'statement : expression'
         if self.debug >= 1: print "got statement"
         t[0] = Stmt(t[1])
 
-    def p_plus_expression(self, t):
-        'expression : expression PLUS expression'
-        if self.debug >= 1: print "got n + n"
-        t[0] = Add((t[1], t[3]))
 
-    def p_int_expression(self, t):
-        'expression : INT'
-        if self.debug >= 1: print "got int"
-        t[0] = Const(t[1])
-
-    def p_expression_group(self, t):
-        'expression : LPAREN expression RPAREN'
-        if self.debug >= 1: print "got parens"
-        t[0] = t[2]
-
-    def p_expression_unary_sub(self, t):
-        'expression : UNARY_SUB expression'
-        if self.debug >= 1: print "got unary sub"
-        t[0] = UnarySub(t[2])
-
-    def p_expression_input(self, t):
-        'expression : INPUT'
-        if self.debug >= 1: print "got input()"
-        t[0] = CallFunc(t[1], 0)
-
+    #
+    # grammar rule
+    #   expression ::= name
+    #
     def p_expression_name(self, t):
         'expression : ID'
         if self.debug >= 1: print "got name"
@@ -62,6 +55,57 @@ class Parser:
         except LookupError:
             print "Undefined name '%s'" % t[1]
             t[0] = 0
+
+
+    #
+    # grammar rule
+    #   expression ::= decimalinteger
+    #
+    def p_int_expression(self, t):
+        'expression : INT'
+        if self.debug >= 1: print "got int"
+        t[0] = Const(t[1])
+
+
+    #
+    # grammar rule
+    #   expression ::= "-" expression
+    #
+    def p_expression_unary_sub(self, t):
+        'expression : UNARY_SUB expression'
+        if self.debug >= 1: print "got unary sub"
+        t[0] = UnarySub(t[2])
+
+
+    #
+    # grammar rule
+    #   expression ::= expression "+" expression
+    #
+    def p_plus_expression(self, t):
+        'expression : expression PLUS expression'
+        if self.debug >= 1: print "got n + n"
+        t[0] = Add((t[1], t[3]))
+
+
+    #
+    # grammar rule
+    #   expression ::= "(" expression ")"
+    #
+    def p_expression_group(self, t):
+        'expression : LPAREN expression RPAREN'
+        if self.debug >= 1: print "got parens"
+        t[0] = t[2]
+
+
+    #
+    # grammar rule
+    #   expression ::= "input" "(" ")"
+    #
+    def p_expression_input(self, t):
+        'expression : INPUT'
+        if self.debug >= 1: print "got input()"
+        t[0] = CallFunc(t[1], 0)
+
 
     def p_error(self, t):
         print "Syntax error at '%s'" % t.value
